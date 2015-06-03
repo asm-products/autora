a5f7f9789c59585c04265958a006b2737f0015ac
Justtal <justtal@gmail.com>
2015-06-03 23:58:01 +0300

    typo

--- public/js/app.js
+++ public/js/app.js
@@ -34,7 +34,7 @@ var auLandingPage = {
         });
 
         $('.subscribe h3').fadeOut(500, function () {
-            $('.subscribe h3').html('Thanks for subscribe to Autora :)').fadeIn(500);
+            $('.subscribe h3').html('Thanks for subscribing! We will be in touch when Autora is ready.').fadeIn(500);
         });
     },
 
