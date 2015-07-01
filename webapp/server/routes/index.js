0fc62bdbd272d85fd9927142877facf245dfbd64
Justtal <justtal@gmail.com>
2015-07-01 23:04:12 +0300

    prod bug 2

--- webapp/server/routes/index.js
+++ webapp/server/routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
     if (app.get('env') === 'development') {
         res.sendfile('./webapp/client/dist/index.html');
     } else {
-        res.sendfile('index.html', { root: __dirname + '/webapp/client/dist' });
+        res.sendfile('index.html', { root: __dirname + '../client/webapp/client/dist' });
         //res.sendfile('./webapp/client/dist/index.html');
     }
 });
