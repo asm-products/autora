59b70af115900a3c193931786a9a73f4f1bbdf5d
Martin Malinda <malindacz@gmail.com>
2015-07-03 10:30:41 +0200

    Expose dist folder

--- webapp/server/routes/index.js
+++ webapp/server/routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 var app = express();
 var path = require('path');
 
+router.get('/*', function(req, res, next) {
+    res.sendfile('./webapp/client/dist'+req.path);
+});
+
 router.get('/', function(req, res, next) {
     if (app.get('env') === 'development') {
         res.sendfile('./webapp/client/dist/index.html');
@@ -16,20 +20,17 @@ router.get('/', function(req, res, next) {
     }
 });
 
-router.get('/assets/client.css', function(req, res, next) {
-    res.sendfile('./webapp/client/dist/assets/client.css');
-});
 
-router.get('/assets/client.js', function(req, res, next) {
-    res.sendfile('./webapp/client/dist/assets/client.js');
-});
+// router.get('/assets/client.js', function(req, res, next) {
+//     res.sendfile('./webapp/client/dist/assets/client.js');
+// });
 
-router.get('/assets/vendor.js', function(req, res, next) {
-    res.sendfile('./webapp/client/dist/assets/vendor.js');
-});
+// router.get('/assets/vendor.js', function(req, res, next) {
+//     res.sendfile('./webapp/client/dist/assets/vendor.js');
+// });
 
-router.get('/assets/vendor.css', function(req, res, next) {
-    res.sendfile('./webapp/client/dist/assets/vendor.css');
-});
+// router.get('/assets/vendor.css', function(req, res, next) {
+//     res.sendfile('./webapp/client/dist/assets/vendor.css');
+// });
 
 module.exports = router;
