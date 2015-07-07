15fbfc0385eef62a44336185fb5835375568be9d
Martin Malinda <malindacz@gmail.com>
2015-07-07 21:51:27 +0200

    Ember simple auth, EmberFire - set up, basic form handling

--- webapp/client/app/user/login/route.js
+++ webapp/client/app/user/login/route.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model: function(params){
+		return null;
+	},
+
+	actions: {
+		didTransition: function(){
+			this.controller.setProperties({'alert': '', 'email' : '', 'password' : ''});
+		}
+	}
 });
---
+++ webapp/client/app/user/signup/controller.js
@@ -0,0 +1,41 @@
+import Ember from 'ember';
+import config from '../../config/environment';
+
+
+export default Ember.Controller.extend({
+	email: '',
+	password: '',
+	password2: '',
+	name: '',
+	alert: '',
+
+	passwordsDontMatch: Ember.computed('password', 'password2', function(){
+		return (this.get('password') !== this.get('password2') && this.get('password2') !== ''); 
+	}),
+
+	actions: {
+		sendSignUpForm: function(){
+			var self = this;
+			var ref = new Firebase(config.firebase);
+			ref.createUser({
+			  email    : this.get('email'),
+			  password : this.get('password')
+			}, function(error, userData) {
+			  if (error) {
+			    console.log("Error creating user:", error);
+			    self.set('alert', {
+			    	type: 'danger',
+			    	message: error
+			    });
+			  } else {
+			    console.log("Successfully created user account with uid:", userData.uid);
+			    self.set('alert', {
+			    	type: 'success',
+			    	message: 'You have a profile now! Congratz!'
+			    });
+			    //ToDO: Create new profile and save it
+			  }
+			});
+		}
+	}
+});
