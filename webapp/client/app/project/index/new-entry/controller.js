00d11e66b4c930531fbb7c5168741f4eac7414c6
MartinMalinda <malindacz@gmail.com>
2015-07-21 08:20:19 +0200

    Successful entry styling, project box styling

--- webapp/client/app/project/index/new-entry/controller.js
+++ webapp/client/app/project/index/new-entry/controller.js
@@ -4,6 +4,8 @@ export default Ember.Controller.extend({
 	newEntry: Ember.computed('model', function(){
 		return {
 			pile: this.get('model').get('firstObject'),
+			project: this.get('model').get('firstObject').get('project'), //DEV MODE
+			author: this.get('session.user'),
 			content: ''
 		}
 	}),
