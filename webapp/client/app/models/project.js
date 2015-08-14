8ae1236a28ef7fe244eb9839598b3d3d52451fee
MartinMalinda <malindacz@gmail.com>
2015-08-14 17:20:18 +0200

    Displaying tags 2

--- webapp/client/app/models/project.js
+++ webapp/client/app/models/project.js
@@ -1,21 +1,21 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  user:  DS.belongsTo('user', {async: true}),
   type: DS.attr('string'),
   inputLength: DS.attr('number'),
   inputType: DS.attr('string'), //paragraph, word..
   languageForm: DS.attr('string'),
-  // tags: DS.attr(),
   name: DS.attr('string'),
   description: DS.attr('string'),
-
-  entries: DS.hasMany('entry'),
-  piles: DS.hasMany('pile'),
-
   open: DS.attr('boolean', {
     defaultValue: true
   }),
+
+  user:  DS.belongsTo('user', {async: true}),
+  tags: DS.hasMany('tag', {async: true}),
+  entries: DS.hasMany('entry'),
+  piles: DS.hasMany('pile'),
+
   createdAt: DS.attr('timestamp'),
   updatedAt: DS.attr('timestamp')
 });
