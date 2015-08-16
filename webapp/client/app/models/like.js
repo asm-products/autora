41a5769af7665176412560dea02a91ae0d2bfd38
MartinMalinda <malindacz@gmail.com>
2015-08-16 13:50:38 +0200

    Another like fix

--- webapp/client/app/models/like.js
+++ webapp/client/app/models/like.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  user: DS.belongsTo('user'),
+  user: DS.belongsTo('user', {async: true}),
   entry: DS.belongsTo('entry', {async: true}),
   
   pile: DS.belongsTo('pile', {async: true}),
