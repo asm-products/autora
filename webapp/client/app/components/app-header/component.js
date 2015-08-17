60d0c070bc77b04e061ec0e011b312a37574bbd9
MartinMalinda <malindacz@gmail.com>
2015-08-17 13:53:00 +0200

    Dropdown styling and change email functionality in User.index.settings

--- webapp/client/app/components/app-header/component.js
+++ webapp/client/app/components/app-header/component.js
@@ -2,5 +2,25 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     classNames: ['app-header'],
-    tagName: 'header'
+    tagName: 'header',
+    showDropdown: false,
+
+    didInsertElement(){
+    	var self = this;
+    	Ember.$(document).click(function(){
+    		self.set('showDropdown', false);
+    	});
+    	Ember.$('.toggleDropdown').click(function(e){
+    		console.log('click?');
+    		self.toggleProperty('showDropdown');
+    		e.stopPropagation();
+    	});
+    },
+
+    actions: {
+    	// toggleDropdown(){
+    	// 	console.log('clickity click?');
+    	// 	this.toggleProperty('showDropdown');
+    	// }
+    }
 });
