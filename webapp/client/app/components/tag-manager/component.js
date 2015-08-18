da04e01085403182df1df5498249f5897e431dc7
MartinMalinda <malindacz@gmail.com>
2015-08-18 13:10:53 +0200

    Tag manager update

--- webapp/client/app/components/tag-manager/component.js
+++ webapp/client/app/components/tag-manager/component.js
@@ -3,12 +3,12 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	//Tags
 	tags: [],
-	rightColSize: Ember.computed('tags.length', function(){
-		return 'col-sm-'+parseInt(12 - 3*this.get('tags.length')); 
-	}),
+	// rightColSize: Ember.computed('tags.length', function(){
+	// 	return 'col-sm-'+parseInt(12 - 3*this.get('tags.length')); 
+	// }),
 	newTagAdd: Ember.computed('newTag',function(){
 		var newTag = this.get('newTag');
-		var regex = /^#.+\ $/;
+		var regex = /^.+\ $/;
 		if(regex.test(newTag)){
 			this.get('tags').pushObject(newTag);
 			this.set('newTag','');
@@ -17,8 +17,13 @@ export default Ember.Component.extend({
 	newTag: '',
 
 	actions: {
-		deleteTag: function(tag){
+		deleteTag(tag){
 			this.get('tags').removeObject(tag);
+		},
+		addTag(){
+			this.get('tags').pushObject(this.get('newTag'));
+			this.set('newTag','');
+			
 		}
 	}
 });
