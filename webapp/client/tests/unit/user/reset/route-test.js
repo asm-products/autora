02bc107e60257b976aa7e519ce95edf0efd85506
MartinMalinda <malindacz@gmail.com>
2015-08-18 12:42:59 +0200

    Generated tests

---
+++ webapp/client/tests/unit/user/reset/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:user/reset', 'Unit | Route | user/reset', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
