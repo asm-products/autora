faeff8a869bd7eba2445571be84fed03bd2b4781
MartinMalinda <malindacz@gmail.com>
2015-08-18 12:43:43 +0200

    Move dropdown to own component to know when it is inserted

--- webapp/client/app/components/app-header/component.js
+++ webapp/client/app/components/app-header/component.js
@@ -5,18 +5,6 @@ export default Ember.Component.extend({
     tagName: 'header',
     showDropdown: false,
 
-    didInsertElement(){
-    	var self = this;
-    	Ember.$(document).click(function(){
-    		self.set('showDropdown', false);
-    	});
-    	Ember.$('.toggleDropdown').click(function(e){
-    		console.log('click?');
-    		self.toggleProperty('showDropdown');
-    		e.stopPropagation();
-    	});
-    },
-
     actions: {
     	// toggleDropdown(){
     	// 	console.log('clickity click?');
