17112e793f530a4dfb01ec4e556b96f218a81d90
MartinMalinda <malindacz@gmail.com>
2015-08-18 12:00:14 +0200

    Password reset

--- webapp/client/app/user/reset/controller.js
+++ webapp/client/app/user/reset/controller.js
@@ -1,4 +1,38 @@
 import Ember from 'ember';
+import Firebase from 'firebase';
+import config from 'client/config/environment';
 
 export default Ember.Controller.extend({
+	email: '',
+	alert: {},
+	isLoading: false,
+
+	actions: {
+		resetPassword(){
+			var ref = new Firebase(config.firebase);
+			var self = this;
+			this.set('isLoading', true);
+			ref.resetPassword({
+			  email: this.get('email'),
+			}, function(error) {
+			  self.set('isLoading', false);
+			  if (error) {
+			    switch (error.code) {
+			      case "INVALID_USER":
+			        self.set('alert.message', 'The specified user account does not exist.');
+			        self.set('alert.type', 'danger');
+			        break;
+			      default:
+			        self.set('alert.message', 'Error resetting password:', error);
+			        self.set('alert.type', 'danger');
+
+			    }
+			  } else {
+			    self.set('alert.message', 'Password reset email sent successfully!');
+			    self.set('alert.type', 'success');
+
+			  }
+			});
+		}
+	}
 });
