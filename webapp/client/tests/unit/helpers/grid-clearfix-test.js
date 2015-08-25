f8847d4c2296b83928538c9cd638c3c60e753ce1
MartinMalinda <malindacz@gmail.com>
2015-08-25 10:19:14 +0200

    gen tests

---
+++ webapp/client/tests/unit/helpers/grid-clearfix-test.js
@@ -0,0 +1,10 @@
+import { gridClearfix } from '../../../helpers/grid-clearfix';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | grid clearfix');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = gridClearfix(42);
+  assert.ok(result);
+});
