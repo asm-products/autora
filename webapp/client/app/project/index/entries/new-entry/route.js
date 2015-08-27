fc82d5cbffb540f2e49ebb3c42ff4e8b78e17a8b
Justtal <justtal@gmail.com>
2015-08-27 23:22:32 +0300

    if not login and click add entry - go to login

--- webapp/client/app/project/index/entries/new-entry/route.js
+++ webapp/client/app/project/index/entries/new-entry/route.js
@@ -9,6 +9,13 @@ export default Ember.Route.extend({
 		// return this.modelFor('project.index.entries');
 	},
 
+	beforeModel: function (transition) {
+		if (!this.get('session.isAuthenticated')) {
+			transition.abort();
+			this.transitionTo('user.login');
+		}
+	},
+
 	afterModel: function(model){
 		if(model.get('length') === 0){
 			this.transitionTo('project.index.entries');
