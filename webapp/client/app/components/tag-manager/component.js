bd362a00b2a764d91f35d54eb1d13c360462df1f
MartinMalinda <malindacz@gmail.com>
2015-09-11 10:39:37 +0200

    Tag name maxlength 20 and tag limit 5

--- webapp/client/app/components/tag-manager/component.js
+++ webapp/client/app/components/tag-manager/component.js
@@ -6,24 +6,34 @@ export default Ember.Component.extend({
 	// rightColSize: Ember.computed('tags.length', function(){
 	// 	return 'col-sm-'+parseInt(12 - 3*this.get('tags.length')); 
 	// }),
-	newTagAdd: Ember.computed('newTag',function(){
+	newTagAdd: Ember.computed('newTag','canAddTags', function(){
 		var newTag = this.get('newTag');
 		var regex = /^.+\ $/;
-		if(regex.test(newTag)){
-			newTag = newTag.substring(0, newTag.length -1); //remove the space at the end
-			this.get('tags').pushObject(newTag);
-			this.set('newTag','');
+		if(this.get('spacelessTag') !== '' && this.get('canAddTags')){
+			if(regex.test(newTag)){
+				newTag = newTag.substring(0, newTag.length -1); //remove the space at the end
+				this.get('tags').pushObject(newTag);
+				this.set('newTag','');
+			}
 		}
 	}),
+	noTagsAdded: Ember.computed.equal('tags.length', 0),
 	newTag: '',
+	spacelessTag: Ember.computed('newTag', function(){
+		return this.get('newTag').replace(/ /g,'');
+	}),
+	canAddTags: Ember.computed.lt('tags.length', 5),
 
 	actions: {
 		deleteTag(tag){
 			this.get('tags').removeObject(tag);
 		},
 		addTag(){
-			this.get('tags').pushObject(this.get('newTag'));
-			this.set('newTag','');
+			var newTag = this.get('spacelessTag');
+			if(newTag !== '' && this.get('canAddTags')){
+				this.get('tags').pushObject(newTag);
+				this.set('newTag','');
+			}
 			
 		}
 	}
