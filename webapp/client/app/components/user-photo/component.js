3e73d4a1b1abf0f34f4378372ca3ed810de2f04f
MartinMalinda <malindacz@gmail.com>
2015-10-18 11:19:26 +0200

    image placeholders

--- webapp/client/app/components/user-photo/component.js
+++ webapp/client/app/components/user-photo/component.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     classNames: ['user-photo'],
     imageService: Ember.inject.service('image'),
+    placeholderUrl: 'http://app.autora.ink/images/profile.png',
     imageUrl: Ember.computed('user.photo', function(){
     	return this.get('imageService').generatePath(this.get('user.photo'),'user',400);
     })
--- webapp/client/app/user/index/controller.js
+++ webapp/client/app/user/index/controller.js
@@ -4,6 +4,6 @@ export default Ember.Controller.extend({
 	imageService: Ember.inject.service('image'),
 	coverPhoto: Ember.computed('model.cover', function () {
 		console.log(this.get('model.cover'),this.get('model.imageHost'));
-        return this.get('imageService').generatePath(this.get('model.cover'),'cover',1024,this.get('model.imageHost')) || 'https://pbs.twimg.com/profile_banners/2870366753/1422565411/1500x500';
+        return this.get('imageService').generatePath(this.get('model.cover'),'cover',1024,this.get('model.imageHost')) || '/images/cover.png';
     })
 });
