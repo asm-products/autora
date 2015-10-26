bb7a5163f95f373a9df9d9ea740d45b00e82045b
MartinMalinda <malindacz@gmail.com>
2015-10-26 10:00:45 +0100

    cleanup

--- webapp/client/app/components/app-header/component.js
+++ webapp/client/app/components/app-header/component.js
@@ -1,30 +1,10 @@
 import Ember from 'ember';
-import InViewportMixin from 'ember-in-viewport';
 
-export default Ember.Component.extend(InViewportMixin, {
+export default Ember.Component.extend({
     classNames: ['app-header'],
     tagName: 'header',
     showDropdown: false,
     action: 'toggleCreate',
-    classNameBindings: ['viewportEntered:active:inactive'],
-
-    didEnterViewport(){
-        console.log('app header in viewport');
-    },
-    didExitViewport(){
-        console.log('app header not in viewport');
-    },
-    didScroll(direction){
-        console.log(direction);
-    },
-
-    viewportOptionsOverride: Ember.on('didInsertElement', function() {
-        console.log('overriding viewport');
-        Ember.setProperties(this, {
-          viewportUseRAF: true,
-          viewportSpy: true,
-        });
-      }),
 
     actions: {
     	// toggleDropdown(){
