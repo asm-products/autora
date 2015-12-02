43da1762ad69515ae8d6bc0e0f8a978f417d2897
MartinMalinda <malindacz@gmail.com>
2015-12-02 22:49:30 +0100

    Projects home sorting

--- webapp/client/app/home/controller.js
+++ webapp/client/app/home/controller.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	projectsSorting: ['updatedAt:desc'],
+	sortedProjects: Ember.computed.sort('model','projectsSorting'),
 	blankProject: {
 		description: '',
 		name: ''
