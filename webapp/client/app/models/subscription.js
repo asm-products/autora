539125300207d6ca2034e397cfdc9bb4a3c1ce88
MartinMalinda <malindacz@gmail.com>
2015-12-13 22:16:45 +0100

    Subscriptions first draft

---
+++ webapp/client/app/models/subscription.js
@@ -0,0 +1,58 @@
+import DS from 'ember-data';
+import TimestampSupport from 'client/mixins/timestamp-support';
+import Ember from 'ember';
+
+
+
+const {computed} = Ember;
+const {attr, belongsTo} = DS;
+
+export default DS.Model.extend(TimestampSupport, {
+
+  type: attr('string'),
+
+  user: belongsTo('user'),
+  // notification: belongsTo('notification'),
+
+  project: belongsTo('project'), // 
+  entry: belongsTo('entry'),
+  pile: belongsTo('pile'),
+
+  cachedPileCount: attr('number', {defaultValue: 0}),
+  cachedEntryCount: attr('number', {defaultValue: 0}),
+  cachedLikeCount: attr('number', {defaultValue: 0}),
+
+  cachedProjectTimestamp: attr('', {defaultValue: Date.now()}),
+  cachedPileTimestamp: attr('', {defaultValue: Date.now()}),
+  cachedEntryTimestamp: attr('', {defaultValue: Date.now()}),
+
+
+  likeCount: computed.alias('entry.likes.count'),
+  competingEntriesCount: computed.alias('pile.competingEntries.count'),
+  successfulEntriesCount: computed.alias('project.entries.count'),
+
+  // childCountingAttr: computed('type', function(){
+  // 	var type = this.get('type');
+
+  // 	if(type === 'project') return 'entries';
+  // 	if(type === 'pile') return 'competingEntries';
+  // 	if(type === 'entry') return 'likes';
+  // }),
+
+  // childCount: computed('childCountingAttr', 'type', function(){
+
+  // 	var type = this.get('type');
+  // 	var capType = Ember.String.capitalize(type);
+  // 	var cachedChildsCount = this.get('cached' + capType + 'Count');
+
+  // 	var objectQuery = type + '.' + this.get('childCountingAttr') + '.length';
+  // 	console.log(objectQuery);
+  // 	var newChildsCount = this.get(objectQuery);
+  // 	return newChildsCount;
+  // }),
+
+  cacheSubscription(){
+
+  }
+
+});
