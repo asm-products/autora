7f1bbd20588c29ab0353b037f75ab409945eb176
MartinMalinda <malindacz@gmail.com>
2015-12-17 10:50:50 +0100

    Modal attachment none

--- webapp/client/app/components/modal-prototype/component.js
+++ webapp/client/app/components/modal-prototype/component.js
@@ -3,10 +3,8 @@ import ModalDialog from 'ember-modal-dialog/components/modal-dialog';
 export default ModalDialog.extend({
   containerClassNames: "modal-prototype",
   hasOverlay: true,
-  // renderInPlace: true,
-  alignment: 'none',
-  attachement: 'none',
-  targetAttachement: 'none',
+  
+  targetAttachment: 'none',
   translucentOverlay: true,
   close: 'toggleCreateProjectModal'
 });
